914bb63bbdafc29986724740cfa175f5ff3121e4
Thiago Braga <thibraga06@gmail.com>
2014-05-02 02:34:36 -0300

    Removendo container do navbar.

--- application/views/_template/navbar.php
+++ application/views/_template/navbar.php
@@ -1,25 +1,23 @@
 <nav id="navbar-top" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <!-- navbar-toggle -->
-            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+    <div class="navbar-header">
+        <!-- navbar-toggle -->
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
 
-            <a href="/" class="navbar-brand">Sociopata</a>
-        </div>
-
-        <nav class="collapse navbar-collapse" role="navigation">
-            <ul class="nav navbar-nav pull-right">
-                <li><a href="/eventos">Eventos</a></li>
-                <li><a href="/midia">Mídia</a></li>
-                <li><a href="/biografia">Biografia</a></li>
-                <li><a href="/links">Links</a></li>
-                <li><a href="/contato">Contato</a></li>
-            </ul>
-        </nav>
+        <a href="/" class="navbar-brand">Sociopata</a>
     </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+        <ul class="nav navbar-nav pull-right">
+            <li><a href="/eventos">Eventos</a></li>
+            <li><a href="/midia">Mídia</a></li>
+            <li><a href="/biografia">Biografia</a></li>
+            <li><a href="/links">Links</a></li>
+            <li><a href="/contato">Contato</a></li>
+        </ul>
+    </nav>
 </nav>
