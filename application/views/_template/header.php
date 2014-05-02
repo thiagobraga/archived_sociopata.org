0187d6a2b418fc7cc0141c7b01f3868783e64073
Thiago Braga <thibraga06@gmail.com>
2014-05-02 02:28:13 -0300

    Adicionado o carrossel em todas as páginas.

--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -1,3 +1,10 @@
-<div class="panel panel-default featured">
-    <img src="assets/images/logo/logo.png" class="img-logo">
-</div>
+<header>
+    <div id="carousel-example-generic" class="carousel carousel-fade slide" data-ride="carousel" data-interval="7000">
+        <div class="carousel-inner">
+            <div class="item active banner-01"></div>
+            <div class="item banner-02"></div>
+        </div>
+
+        <img src="assets/images/logo/logo.png" class="img-logo" />
+    </div>
+</header>
