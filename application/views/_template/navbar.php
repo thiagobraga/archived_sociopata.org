794816883f15b0ae885bd6a09daaf2eed1b23bd7
Thiago Braga <thibraga06@gmail.com>
2014-05-26 23:39:24 -0300

    Removendo ícones de redes sociais causando bug indesejado.
    Implementar melhoria com mais tempo para evitar bugs em vários navegadores.

--- application/views/_template/navbar.php
+++ application/views/_template/navbar.php
@@ -9,21 +9,15 @@
         </button>
 
         <a href="/" class="navbar-brand">Sociopata</a>
-
-        <ul class="nav navbar-nav navbar-icons pull-right">
-            <li><a href="//facebook.com/sociopatabr" target="_blank"><span class="fa fa-facebook"></span></a></li>
-            <li><a href="//youtube.com/sociopataweb" target="_blank"><span class="fa fa-youtube-play"></span></a></li>
-            <li><a href="//twitter.com/sociopatabr" target="_blank"><span class="fa fa-twitter"></span></a></li>
-        </ul>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
         <ul class="nav navbar-nav pull-right">
-            <li><a href="/eventos">Eventos</a></li>
-            <li><a href="/midia">Mídia</a></li>
-            <li><a href="/biografia">Biografia</a></li>
-            <li><a href="/links">Links</a></li>
-            <li><a href="/contato">Contato</a></li>
+            <li <?php if ($class == 'eventos') { ?> class="active" <?php } ?>><a href="/eventos">Eventos</a></li>
+            <li <?php if ($class == 'midia') { ?> class="active" <?php } ?>><a href="/midia">Mídia</a></li>
+            <li <?php if ($class == 'biografia') { ?> class="active" <?php } ?>><a href="/biografia">Biografia</a></li>
+            <li <?php if ($class == 'links') { ?> class="active" <?php } ?>><a href="/links">Links</a></li>
+            <li <?php if ($class == 'contato') { ?> class="active" <?php } ?>><a href="/contato">Contato</a></li>
         </ul>
     </nav>
 </nav>
