6d076104d04bffa934e1ffbd3dac7ba5bb64c5d7
Thiago Braga <thibraga06@gmail.com>
2014-11-26 11:34:31 -0200

    Including Sociopata configurations file.

---
+++ application/config/sociopata.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * sociopata.php
+ *
+ * Sociopata Configuration File
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  Music
+ * @package   Sociopata
+ * @author    Thiago Braga <contato@thiagobraga.org>
+ * @copyright 1997-2005 The PHP Group
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   GIT: $Id$
+ * @link      http://sociopata.org
+ * @since     File available since Release 0.0.0
+ */
+
+defined('BASEPATH') || exit('No direct script access allowed');
+
+$config['fb_admins'] = array(
+    '799997746730203', // Thiago Braga
+    '100000165014474', // Gustavo Panurge
+    '100001306820569' // Birão Spoldari
+);
+
