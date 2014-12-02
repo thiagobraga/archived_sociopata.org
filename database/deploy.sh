58bb7f12133308a465a4fd72903b7b1bdd6127aa
Thiago Braga <thibraga06@gmail.com>
2014-12-02 03:23:00 -0200

    Removing deploy.sh from versioning

--- database/deploy.sh
+++
@@ -1,2 +0,0 @@
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source D:/Projetos/Vhosts/sociopata/database/schema.sql" -v -v -v -t --default-character-set=utf8
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source D:/Projetos/Vhosts/sociopata/database/data.sql" -v -v -v -t --default-character-set=utf8
