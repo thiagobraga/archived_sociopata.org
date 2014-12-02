818814ece7b0a11a18a34a756928d79ec5f2954a
Thiago Braga <thibraga06@gmail.com>
2014-12-02 03:31:06 -0200

    Using Lightbox on news content

--- application/views/home/noticias.php
+++ application/views/home/noticias.php
@@ -5,7 +5,10 @@
         <?php foreach ($noticias as $noticia) { ?>
             <article class="media row">
                 <div class="media-object col-xs-5">
-                    <img src="assets/images/<?php echo $noticia->imagem ?>" class="img-responsive" />
+                    <a href="assets/images/<?php echo $noticia->imagem ?>" data-lightbox="image-1" data-title="<h6><?php echo $evento->nome ?> - <small><?php echo date('d/m/Y', strtotime($evento->data)) ?></small></h6>">
+                        <img src="assets/images/<?php echo $noticia->imagem ?>" class="img-responsive" />
+                    </a>
+
                 </div>
                 <div class="media-body col-xs-7">
                     <h6 class="media-heading"><?php echo $noticia->nome ?></h6>
