3012cceaab3b9fd99bba31c527cda4ebe05140a7
Thiago Braga <thiago@institutosoma.org.br>
2015-02-12 13:51:08 -0200

    fixed untracked files

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # MySQL
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source ./db/schema.sql" -v -t --default-character-set=utf8
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source ./db/data.sql" -v -t --default-character-set=utf8
+mysql -uroot -p"am0s r3c4d" -e "source ./db/schema.sql" -v -t --default-character-set=utf8
+mysql -uroot -p"am0s r3c4d" -e "source ./db/data.sql" -v -t --default-character-set=utf8
 
 # Install node.js modules from package.json
 npm install --save-dev
