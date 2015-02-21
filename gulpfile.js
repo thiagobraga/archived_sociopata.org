7314c2cfca46ccdedc625e92c9c5fefd6f644df9
Thiago Braga <thibraga06@gmail.com>
2015-02-21 03:26:43 -0200

    Almost perfect gulpfile for SASS, JS, images and browser reload

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var gulp       = require('gulp'),
   imagemin     = require('gulp-imagemin'),
   pngquant     = require('imagemin-pngquant'),
   browserSync  = require('browser-sync'),
+  reload       = browserSync.reload,
 
   bsFiles = [
     'public/css/**.min.css',
@@ -24,59 +25,63 @@ var gulp       = require('gulp'),
   ],
 
   fonts = [
-    './bower_components/font-awesome/fonts/**.*',
-    './bower_components/bootstrap-sass/assets/fonts/bootstrap/**.*',
-    './src/fonts/**.*'
+    'bower_components/font-awesome/fonts/**.*',
+    'bower_components/bootstrap-sass/assets/fonts/bootstrap/**.*',
+    'src/fonts/**.*'
   ],
 
   js_files = [
-    './bower_components/jquery/dist/jquery.js',
-    './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-    './bower_components/lightbox/js/lightbox.js',
-    './bower_components/OwlCarousel/owl-carousel/owl.carousel.js',
-    './bower_components/jQuery-linkify/dist/jquery.linkify.js',
-    './bower_components/soundmanager/script/soundmanager2-nodebug.js',
-    './src/js/**/*.js'
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+    'bower_components/lightbox/js/lightbox.js',
+    'bower_components/OwlCarousel/owl-carousel/owl.carousel.js',
+    'bower_components/jQuery-linkify/dist/jquery.linkify.js',
+    'bower_components/soundmanager/script/soundmanager2-nodebug.js',
+    'src/js/**/*.js'
   ];
 
 // Styles
 gulp.task('styles', function () {
-  return gulp.src('./src/scss/styles.scss')
+  return gulp.src('src/scss/styles.scss')
     .pipe(plumber())
-    .pipe(sass({ style: 'expanded' }))
+    .pipe(sass())
+    .on('error', function (err) {
+      console.log(err.message);
+      this.emit('end');
+    })
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest('public/css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('./public/css'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(gulp.dest('public/css'))
+    .pipe(reload({ stream: true }));
 });
 
 // Scripts
 gulp.task('scripts', function () {
   return gulp.src(js_files)
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./public/js'))
+    .pipe(gulp.dest('public/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('./public/js'));
+    .pipe(gulp.dest('public/js'));
 });
 
 // Images
 gulp.task('images', function () {
-  return gulp.src('./src/images/**/*')
+  return gulp.src('src/images/**/*')
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{ removeViewBox: false }],
       use: [pngquant({ quality: 80 })]
     }))
-    .pipe(gulp.dest('./public/images'));
+    .pipe(gulp.dest('public/images'));
 });
 
 // Fonts
 gulp.task('fonts', function () {
   return gulp.src(fonts)
-    .pipe(gulp.dest('./public/fonts'));
+    .pipe(gulp.dest('public/fonts'));
 });
 
 // BrowserSync
@@ -94,9 +99,9 @@ gulp.task('browser-sync', function () {
 
 // Watch
 gulp.task('watch', function () {
-  gulp.watch('./src/scss/**/*.scss', ['styles']);
-  gulp.watch('./src/js/**/*.js', ['scripts']);
-  gulp.watch('./src/images/**.*', ['images']);
+  gulp.watch('src/scss/**/*.scss', ['styles']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/images/**/*', ['images']);
 });
 
 // Default
