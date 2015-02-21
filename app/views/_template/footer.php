c880f686ea1592b7a0be7cfa5864d182f08fb150
Thiago Braga <thibraga06@gmail.com>
2015-02-21 03:27:48 -0200

    Refactoring and correcting images folder path

--- app/views/_template/footer.php
+++ app/views/_template/footer.php
@@ -1,3 +1,10 @@
-<footer>
-    <a href="/" class="footer-brand">Sociopata</a>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <br/>
+                <img src="http://placehold.it/1140x400/121212/666666&text=Footer">
+            </div>
+        </div>
+    </div>
 </footer>
--- app/views/biografia/biografia.php
+++ app/views/biografia/biografia.php
@@ -1,12 +1,10 @@
 <div class="container">
-    <section class="<?php echo $controller ?>">
-        <div class="row">
-            <div class="col-xs-8">
-                <?php $this->load->view('biografia/release') ?>
-            </div>
-            <div class="col-xs-4">
-                <?php $this->load->view('biografia/integrantes') ?>
-            </div>
+    <div class="row">
+        <div class="col-xs-8">
+            <?php $this->load->view('biografia/release') ?>
         </div>
-    </section>
+        <div class="col-xs-4">
+            <?php $this->load->view('biografia/integrantes') ?>
+        </div>
+    </div>
 </div>
