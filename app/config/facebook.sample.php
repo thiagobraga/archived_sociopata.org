00b4da97b43a1f68af93779471b8e61d813e1c71
Thiago Braga <thibraga06@gmail.com>
2015-02-22 19:31:01 -0300

    Adding sample files

---
+++ app/config/facebook.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('BASEPATH') || exit('No direct script access allowed');
+
+// Chave do aplicativo para acessar o facebook (Secret key)
+$config['appID']  = 'your_app_id';
+$config['secret'] = 'your_secret_key';
+$config['cookie'] = true;
+$config['oauth']  = true;
